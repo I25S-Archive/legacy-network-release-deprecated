ae8b96ae56fbb6268ca9b32ce3026fe44e8fcd38
carsten <cr@tagion.org>
2020-07-13 13:06:23 +0300

    Submodules uodated

---
+++ ddoc_builder.mk
@@ -0,0 +1,40 @@
+DDOCREPO?=${shell git rev-parse --show-toplevel}
+
+ifndef DDOCHELP
+DDOCHELP:=help-ddoc
+HELP+=$(DDOCHELP)
+else
+CADRYDOOT:=$(DDOCREPO)/candydoc/
+DOCBEGIN:= $(DDOCREPO)/begin.html
+DOCEND:= $(DDOCREPO)/end.html
+DDOCSCRIPT:=$(DDOCREPO)/scripts/ddocmodule.pl
+DDOCINDEX?=$(DDOCROOT)/index.html
+DDOCWORKDIR?=/tmp/$(GIT_HASH)/
+
+help-ddoc:
+	@echo "make ddoc      : Creates source documentation"
+	@echo
+
+
+ddoc: $(DDOCINDEX)
+
+$(DDOCINDEX): $(DDOCWORKDIR) $(DDOCMODULES)
+	@echo "########################################################################################"
+	@echo "## Creating DDOC"
+	${PRECMD}cp -a $(CADRYDOOT) $(DDOCROOT)
+	$(PRECMD)$(DC) ${INCFLAGS} $(DDOCFLAGS) $(DDOCFILES) $(DFILES) $(DD)$(DDOCWORKDIR)
+	$(PRECMD)cat $(DDOCBEGIN) $(DDOCWORKDIR)/*.html $(DOCBEGIN) > $@
+
+
+$(DDOCMODULES): $(DFILES)
+	$(PRECMD)echo $(DFILES) | $(DDOCSCRIPT) > $@
+
+$(DDOCWORKDIR):
+	mkdir -p $@
+
+clean-ddoc:
+	rm -fR $(DDOCWORKDIR)
+
+CLEANER+=clean-ddoc
+
+endif
